75611f12d48715d3c2fe17b611409ed7650af584
Gustavo Wagner <gugawag@gmail.com>
2023-10-05 15:23:25 -0300

    v1-inicio

---
+++ src/main/java/org/example/Aluno.java
@@ -0,0 +1,49 @@
+package org.example;
+
+public class Aluno {
+    private int id;
+    private String nome;
+    private int idade;
+    private String matricula;
+    private String cpf;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
